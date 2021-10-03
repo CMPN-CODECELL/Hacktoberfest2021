ca7f23eb3a72223ed84bee503c4b54ff771e1024
Ashwin Pansare <ashwinpansare6@gmail.com>
2021-10-03 18:16:47 +0530

    Merge Sort Algorithm in Java

--- Algorithms/Java/Sorting Algorithms/MergeSort.java
+++ Algorithms/Java/Sorting Algorithms/MergeSort.java
@@ -1,15 +1,23 @@
+//Time complexity of O(n*logn) in worst, average and best case
+
+/*
+It is a divide and conquer algorithm which divides the array into two subarrays at
+each step until both subarrays have size of one and then arranges the elements
+while merging both the arrays
+*/
+
 import java.util.*;
 import java.util.regex.*;
 class MergeSort{
   static int n;
   public static void main(String args[]){
-    System.out.println("Enter the number of elements in the array");
+    System.out.println("Enter the size of the array");//size of array
     Scanner sc = new Scanner(System.in);
     while(true){
       try{
         n = sc.nextInt();
         break;
-      }catch(InputMismatchException e)
+      }catch(InputMismatchException e)//If user doesn't enter an integer
       {
         sc.nextLine();
         System.out.println("Please enter an integer");
@@ -30,19 +38,18 @@ class MergeSort{
         }
         else
         {
-          System.out.println("Please enter an option between 1 and 2");
-          System.out.println("Please");
+          System.out.println("Please enter either 1 or 2");
           continue;
         }
       }catch(InputMismatchException e){
         sc.nextLine();
-        System.out.println("Please enter an option between 1 and 2");
+        System.out.println("Please enter either 1 or 2");
         continue;
       }
     }
     if(c==1)//integer array
     {
-      int arr[] = new int[n];
+      int array[] = new int[n];
       System.out.println("Enter the elements of the array");
       for(int i=0;i<n;i++)
       {
@@ -50,7 +57,7 @@ class MergeSort{
         System.out.print("Element "+(i+1)+" :");
         try   //checks if user has entered integer, else asks for input again
         {
-          arr[i] = sc.nextInt();
+          array[i] = sc.nextInt();
           continue;
         }
         catch(InputMismatchException e){
@@ -60,21 +67,21 @@ class MergeSort{
           continue;
         }
       }
-      sort(arr,0,n-1);
+      sort(array,0,n-1);
       System.out.println();
       System.out.println("Sorted array: ");
-      System.out.println(Arrays.toString(arr));
+      System.out.println(Arrays.toString(array));
     }
     else//character array
     {
-      char arr[] = new char[n];
+      char array[] = new char[n];
       System.out.println("Enter the elements of the array");
       for(int i=0;i<n;i++)
       {
         System.out.println();
         System.out.print("Element "+(i+1)+" :");
         String str = sc.next(".");
-        arr[i] = str.charAt(0);//takes only one character
+        array[i] = str.charAt(0);//takes only one character
         if(!Pattern.matches("[a-zA-Z]",str))
         {
           System.out.println("Please enter a valid character");
@@ -82,109 +89,109 @@ class MergeSort{
           continue;
         }
       }
-      sort(arr,0,n-1);
+      sort(array,0,n-1);
       System.out.println();
       System.out.println("Sorted array: ");
-      System.out.println(Arrays.toString(arr));
+      System.out.println(Arrays.toString(array));
     }
   }
 
-  static void sort(int arr[], int l, int r)//method for integer arrays
+  static void sort(int array[], int l, int h)//method for integer arrays
   {
-    if (l < r)
+    if (l < h)
     {
-      int m =l+(r-l)/2;
-      sort(arr, l, m);
-      sort(arr, m+1, r);
-      merge(arr, l, r);
+      int m =l+(h-l)/2;
+      sort(array, l, m);
+      sort(array, m+1, h);
+      merge(array, l, h);
     }
   }
-  static void sort(char arr[], int l, int r)//method for character arrays
+  static void sort(char array[], int l, int h)//method for character arrays
   {
-    if (l < r)
+    if (l < h)
     {
-      int m =l+(r-l)/2;
-      sort(arr, l, m);
-      sort(arr, m+1, r);
-      merge(arr, l, r);
+      int m =(l+h)/2;
+      sort(array, l, m);
+      sort(array, m+1, h);
+      merge(array, l, h);
     }
   }
 
-  static void merge(int arr[], int l, int r)
+  static void merge(int array[], int l, int h)
   {
-    int m = (l+r)/2;
-    int n1 = m - l + 1;
-    int n2 = r - m;
-    int L[] = new int[n1];//left subarray
-    int R[] = new int[n2];//right subarray
-    for (int i=0;i<n1;i++)
-      L[i] = arr[l+i];
-    for (int j=0;j<n2;j++)
-      R[j] = arr[m+1+j];
+    int m = (l+h)/2;
+    int l_len = m-l+1;
+    int r_len = h-m;
+    int l_arr[] = new int[l_len];//left subarray
+    int r_arr[] = new int[r_len];//right subarray
+    for (int i=0;i<l_len;i++)
+      l_arr[i] = array[l+i];
+    for (int i=0;i<r_len;i++)
+      r_arr[i] = array[m+i+1];
 
-    int i = 0, j = 0;
-    int k = l;
-    while (i<n1 && j<n2) {//adding the elements in the merged array
-      if (L[i] <= R[j])
+    int i=0, j=0;
+    int array_ind=l;
+    while (i<l_len && j<r_len) {//adding the elements in the merged array
+      if (l_arr[i] <= r_arr[j])
       {
-        arr[k] = L[i];
+        array[array_ind] = l_arr[i];
         i++;
       }
       else
       {
-        arr[k] = R[j];
+        array[array_ind] = r_arr[j];
         j++;
       }
-      k++;
+      array_ind++;
     }
-    while (i < n1) {//adding the remaining elements from left subarray
-      arr[k] = L[i];
+    while (i < l_len) {//adding the remaining elements from left subarray
+      array[array_ind] = l_arr[i];
       i++;
-      k++;
+      array_ind++;
     }
-    while (j < n2) {//adding the remaining elements from right subarray
-      arr[k] = R[j];
+    while (j < r_len) {//adding the remaining elements from right subarray
+      array[array_ind] = r_arr[j];
       j++;
-      k++;
+      array_ind++;
     }
   }
 
-  static void merge(char arr[], int l, int r)
+  static void merge(char array[], int l, int h)
   {
-    int m = (l+r)/2;
-    int n1 = m - l + 1;
-    int n2 = r - m;
-    char L[] = new char[n1];//left subarray
-    char R[] = new char[n2];//right subarray
-    for (int i=0;i<n1;i++)
-      L[i] = arr[l + i];
-    for (int j=0;j<n2;j++)
-      R[j] = arr[m+1+j];
+    int m = (l+h)/2;
+    int l_len = m-l+1;
+    int r_len = h-m;
+    char l_arr[] = new char[l_len];//left subarray
+    char r_arr[] = new char[r_len];//right subarray
+    for (int i=0;i<l_len;i++)
+      l_arr[i] = array[l + i];
+    for (int i=0;i<r_len;i++)
+      r_arr[i] = array[m+i+1];
 
-    int i = 0, j = 0;
-    int k = l;
-    while (i<n1 && j<n2) {//adding the elements in the merged array
-      if (Character.compare(L[i],R[j])<=0)
+    int i=0, j=0;
+    int array_ind = l;
+    while (i<l_len && j<r_len) {//adding the elements in the merged array
+      if (Character.compare(l_arr[i],r_arr[j])<=0)
       {
-        arr[k] = L[i];
+        array[array_ind] = l_arr[i];
         i++;
       }
       else
       {
-        arr[k] = R[j];
+        array[array_ind] = r_arr[j];
         j++;
       }
-      k++;
+      array_ind++;
     }
-    while (i < n1) {//adding the remaining elements from left subarray
-      arr[k] = L[i];
+    while (i < l_len) {//adding the remaining elements from left subarray
+      array[array_ind] = l_arr[i];
       i++;
-      k++;
+      array_ind++;
     }
-    while (j < n2) {//adding the remaining elements from right subarray
-      arr[k] = R[j];
+    while (j < r_len) {//adding the remaining elements from right subarray
+      array[array_ind] = r_arr[j];
       j++;
-      k++;
+      array_ind++;
     }
   }
 }
